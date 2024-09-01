5cd120570ce3927d37a898cf3c95899f0092028f
Sweeney Ngo <sweeneyngo@proton.me>
2024-08-31 18:32:47 -0700

    Update workflow

--- dist/server/server-reference-manifest.js
+++ dist/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"r0CQ+4N3NkbALH96Yx4+X0fdxJHQmhExiDEOyOZymZU=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"gcGs6FF09kIC8AAhgDeLmcjVuBAs7cQEgWR6NqjkjD4=\"\n}"
--- dist/static/chunks/webpack.js
+++ dist/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "746ec6aa363898e4"; }
+/******/ 		__webpack_require__.h = function() { return "76f5159f06b98e8e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
